522963a3da5b6d989285070b4fa452e89794af14
IBM <IBM@DEVOPS_21>
2018-07-30 15:39:15 +0530

    pushed

---
+++ testt/src/test/java/Runner1/Runner1.java
@@ -0,0 +1,11 @@
+package Runner1;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="feature",glue="StepDefinition")
+public class Runner1 {
+
+}
